57b796067edae6a3776e1f0cb75262bce9c3b875
Michael Naumov <mnaoumov@gmail.com>
2024-01-29 17:39:41 -0600

    Ensure services are restarted

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ pip3 install -r requirements.txt
 sudo cp bot.service /etc/systemd/system/bot.service
 sudo systemctl daemon-reload
 sudo systemctl enable bot
-sudo systemctl start bot
+sudo systemctl restart bot
 
 sudo cp bot-webhook.service /etc/systemd/system/bot-webhook.service
 sudo systemctl daemon-reload
 sudo systemctl enable bot-webhook
-sudo systemctl start bot-webhook
+sudo systemctl restart bot-webhook
