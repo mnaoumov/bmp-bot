3ecc2a471eaaac44f9a8f2c2de416c9fd3d069ba
Michael Naumov <mnaoumov@gmail.com>
2024-01-29 15:49:54 -0600

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip
+pip3 install -r requirements.txt
+sudo cp bot.service /etc/systemd/system/bot.service
+sudo systemctl daemon-reload
+sudo systemctl enable bot
+sudo systemctl start bot
