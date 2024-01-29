239ff260acc07e57374f96060822650ca4d2e93e
Michael Naumov <mnaoumov@gmail.com>
2024-01-29 16:46:59 -0600

    Add logging

--- webhook.py
+++ webhook.py
@@ -2,7 +2,9 @@ from flask import Flask, request, abort
 import subprocess
 import hmac
 import hashlib
+import logging
 import os
+import sys
 from dotenv import load_dotenv
 
 def get_env(key: str) -> str:
@@ -11,7 +13,26 @@ def get_env(key: str) -> str:
         raise Exception(f'Environment variable {key} is not set')
     return value
 
+def handle_unhandled_exceptions(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger.error("Unhandled exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+
 def main():
+    global logger
+    logger = logging.getLogger('my_logger')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='!log-webhook.txt', encoding='utf-8')
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    sys.excepthook = handle_unhandled_exceptions
+
     app = Flask(__name__)
     load_dotenv()
     WEBHOOK_SECRET = get_env('WEBHOOK_SECRET')
