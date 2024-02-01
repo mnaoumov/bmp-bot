caf90dc663c690ada2d71e99affaa1616016ff5d
Michael Naumov <mnaoumov@gmail.com>
2024-02-01 16:47:46 -0600

    Add missing serialization

--- main.py
+++ main.py
@@ -37,6 +37,17 @@ class User:
             self.first_name = first_name
             self.last_name = last_name
 
+    def to_dict(self) -> dict:
+        """
+        for JSON serialization
+        """
+
+        return {
+            "id": self.id,
+            "username": self.username,
+            "first_name": self.first_name,
+            "last_name": self.last_name
+        }
 
 class BmpBot:
     """
