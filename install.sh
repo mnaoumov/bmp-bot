024e47350f915596bc30663ef77a8eedcbdc0091
Michael Naumov <mnaoumov@gmail.com>
2025-10-13 08:10:30 -0600

    chore: use venv

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y python3 python3-pip
-pip3 install -r requirements.txt
+sudo apt-get install -y python3 python3-pip python3-venv
+python3 -m venv .venv
+. .venv/bin/activate
+python -m pip install -U pip
+pip install -r requirements.txt
 
 sudo cp bot.service /etc/systemd/system/bot.service
 sudo systemctl daemon-reload
